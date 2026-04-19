c1209e7f51ef9ca240f7ade6f80e2c758e61597a
Junghyun99 <swqasw12345@gmail.com>
2026-04-19 21:32:09 +0900

    refactor(fetcher): unpack (ohlcv, vix, dividends) and return flat Close DataFrame

--- src/backtest/fetcher.py
+++ src/backtest/fetcher.py
@@ -12,6 +12,10 @@ def download_historical_data(
     """
     백테스트용 종가 데이터 다운로드 (캐시 활용).
 
+    BacktestDataCache.get_data()는 (OHLCV, VIX, Dividends) 튜플을 반환한다.
+    이 어댑터는 OHLCV의 MultiIndex에서 Close 레벨만 추출해 티커를 컬럼으로 하는
+    flat DataFrame을 반환한다. 상장일 보정과 ffill은 캐시 내부에서 이미 적용된다.
+
     Args:
         tickers: 종목 코드 리스트
         start_date: 시작일 'YYYY-MM-DD'
@@ -24,4 +28,8 @@ def download_historical_data(
     if cache is None:
         cache = BacktestDataCache()
 
-    return cache.get_data(tickers, start_date, end_date)
+    ohlcv, _, _ = cache.get_data(tickers, start_date, end_date)
+    close_df = ohlcv["Close"]
+    if isinstance(close_df, pd.Series):
+        close_df = close_df.to_frame(name=tickers[0])
+    return close_df
